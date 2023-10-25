8d42df8fe72f497272865fe85d0d4357af53754e
stenlyrachmad87 <tenleegames@gmail.com>
2023-10-25 13:13:49 +0700

    fix small mistake

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Test Masuk',
+      title: 'Test Masuk Kerja',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
